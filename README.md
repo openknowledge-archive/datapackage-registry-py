fd78db1a2f675ba4a0af29dffc6893538155a354
Vitor Baptista <vitor@vitorbaptista.com>
2016-04-18 11:25:16 +0100

    Revert "Merge pull request #13 from okfn/deprecate-repository"
    This reverts commit 33d55899d2d65e810287ce8c68f4caeae61eb4ce, reversing
    changes made to fad2efdc44242d490c6a4a913f481aa91d96690a.
    
    I'm reverting this to be able to push a new deprecation notice on PyPI.
    
    Conflicts:
    	README.md

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-# datapackage-registry-py
+**This project is **DEPRECATED**. You should use
+<https://github.com/datapackages/datapackage-py> instead. This repository is
+just for historical purposes.**
 
-This project is **DEPRECATED**. You should use
-<https://github.com/datapackages/datapackage-py> instead.
+datapackage-registry-py
+=======================
+
+A Python library for working with a Data Package Registry.
+
+It comes with a cached version of the registry and schemas from
+<https://github.com/dataprotocols/schemas>.
+
+Usage
+-----
+
+```python
+import datapackage_registry
+
+# get the default registry objects
+registry = datapackage_registry.Registry()
+
+# see the available profiles in the registry
+print(registry.available_profiles)
+# {
+#     'base': {
+#         'id': 'base',
+#         'schema': 'https://rawgit.com/dataprotocols/schemas/master/data-package.json',
+#         'specification': 'http://dataprotocols.org/data-packages',
+#         'title': 'Data Package'
+#     },
+#     'tabular': {
+#         'id': 'tabular',
+#         'schema': 'https://rawgit.com/dataprotocols/schemas/master/tabular-data-package.json',
+#         'specification': 'http://dataprotocols.org/tabular-data-package/',
+#         'title': 'Tabular Data Package'
+#    }
+# }
+
+# get a profile by its id
+base_profile = registry.get('base')
+```
+
+If you’d like to use a custom registry, you can pass its URL or local
+path to the `Registry()` constructor, as in:
+
+```python
+import datapackage_registry
+
+registry = datapackage_registry.Registry('http://someplace.com/my-registry.csv')
+```
+
+If you’d like to get a schema that's not in the registry, you can pass its URL
+or local path to `Registry().get_external()` method, as in:
+
+```python
+import datapackage_registry
+registry = datapackage_registry.Registry()
+
+try:
+  schema = registry.get_external('http://someplace.com/schema.json')
+except datapackage_registry.exceptions.DataPackageRegistryException:
+  pass  # handle error
+```
+
+Developer notes
+---------------
+
+These notes are intended to help people that want to contribute to this
+package itself. If you just want to use it, you can safely ignore this.
+
+### Updating the local schemas cache
+
+We cache the schemas from <https://github.com/dataprotocols/schemas>
+using git-subtree. To update it, use:
+
+    git subtree pull --prefix datapackage_registry/schemas https://github.com/dataprotocols/schemas.git master --squash
---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import io
+import os
+import fnmatch
+
+from setuptools import setup, find_packages
+
+description = "A Python library for working with a Data Package Registry."
+with io.open('README.md') as readme:
+    long_description = ''.join(
+        filter(lambda x: 'https://travis-ci.org/' not in x,
+               readme.readlines()))
+
+dependencies = [
+    'requests>=2.8.0',
+    'six>=1.10.0',
+]
+
+
+def schema_files():
+    '''Return all CSV and JSON files paths in datapackage_registry/schemas
+
+    The paths are relative to ./datapackage_registry
+    '''
+    def recursive_glob(path, patterns):
+        results = []
+        for base, dirs, files in os.walk(path):
+            matching_files = []
+            for pattern in patterns:
+                matching_files.extend(fnmatch.filter(files, pattern))
+            results.extend(os.path.join(base, f) for f in matching_files)
+        return results
+
+    base_folder = 'datapackage_registry'
+    remove_base_folder = lambda path: path[len(base_folder) + 1:]
+
+    path = os.path.join(base_folder, 'schemas')
+    files_paths = recursive_glob(path, ['*.csv', '*.json'])
+
+    return [remove_base_folder(f) for f in files_paths]
+
+setup(
+    name='datapackage-registry',
+    version='0.4.0',
+    url='https://github.com/okfn/datapackage-registry-py',
+    license='MIT',
+    description=description,
+    long_description=long_description,
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_dir={'datapackage_registry': 'datapackage_registry'},
+    package_data={'datapackage_registry': schema_files()},
+    install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
